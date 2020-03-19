282f5c4edb6c744312e5d8924ed8523c2a55b727
twilio-ci <twilio-ci@twilio.com>
2020-03-19 18:34:55 +0000

    0.5.1

--- test/voice/index.js
+++
@@ -1,7 +0,0 @@
-require('./inbound/spec/Reservation');
-require('./inbound/spec/Supervisor');
-require('./outbound/spec/Reservation');
-require('./outbound/spec/TaskTransferToWorker');
-require('./outbound/spec/Task');
-require('./outbound/spec/TaskTransferToQueue');
-
